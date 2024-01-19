ffbc692da38a4b84a78557a6624751107edded3b
Shawn Ruby <snyrby@gmail.com>
2024-01-18 16:28:27 -0800

    Finished navigation sidebar and create server modal

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,7 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
-import NavigationAction from "./navigation-action";
+import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavigationItem } from "./navigation-item";

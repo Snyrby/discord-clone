445c3f16cf50de3e59dae27e21c8cfc6cdd11413
Shawn Ruby <snyrby@gmail.com>
2024-04-17 07:02:42 -0700

    Made some progress for sockets

--- types.ts
+++ types.ts
@@ -6,3 +6,11 @@ import { Server as SocketIOServer } from "socket.io";
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
 };
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer;
+    };
+  };
+};

61fe907629b7b795a33759780c0ac7ad5c9fe3af
Shawn Ruby <snyrby@gmail.com>
2024-05-03 06:59:26 -0700

    Small progress to chat input

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,6 +1,37 @@
 "use client";
 
-export const ChatInput = () => {
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+type ChatInputProps = {
+  apiUrl: string;
+  query: Record<string, any>;
+  name: string;
+  type: "conversation" | "channel";
+}
+
+const formSchema = z.object({
+  content: z.string().min(1),
+})
+
+export const ChatInput = ({
+  apiUrl,
+  query,
+  name,
+  type
+}: ChatInputProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: "",
+    }
+  })
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    console.log(value);
+  }
   return (
     <div>ChatInput</div>
   )

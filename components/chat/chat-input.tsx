eef1ec0de1807c1ebcb69fe04f3f8d82f55c6ae7
Shawn Ruby <snyrby@gmail.com>
2024-05-08 07:09:16 -0700

    Small change

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -8,6 +8,7 @@ import { Plus, Smile } from "lucide-react";
 import { Input } from "../ui/input";
 import axios from "axios";
 import qs from "query-string";
+import { useModal } from "@/hooks/use-modal-store";
 
 type ChatInputProps = {
   apiUrl: string;
@@ -21,6 +22,7 @@ const formSchema = z.object({
 });
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,8 +35,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       const url = qs.stringifyUrl({
         url: apiUrl,
         query,
-      })
-      await axios.post(url, values)
+      });
+      await axios.post(url, values);
     } catch (error) {
       console.log(error);
     }
@@ -51,8 +53,9 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {}}
-                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
+                    className="absolute 
+                    top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
                   </button>

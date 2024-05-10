b21478719ecaefc22ad0cbcb9fe78b8c8c726e53
Shawn Ruby <snyrby@gmail.com>
2024-05-10 06:56:20 -0700

    Finished chat file upload and pdf visual

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,7 +2,7 @@
 
 import { UploadDropzone } from "@/lib/uploadthing";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import "@uploadthing/react/styles.css";
@@ -20,7 +20,7 @@ export const FileUpload = ({
   onChange,
   value,
   endpoint,
-  edit
+  edit,
 }: FileUploadProps) => {
   const [imageId, setImageId] = useState("");
   const fileType = value?.split(".").pop();
@@ -30,8 +30,10 @@ export const FileUpload = ({
         <Image src={value} fill alt="Upload" className="rounded-full" />
         <button
           onClick={() => {
-            onChange(""); 
-            {edit !== true && deleteValue(imageId)};
+            onChange("");
+            {
+              edit !== true && deleteValue(imageId);
+            }
           }}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
           type="button"
@@ -41,6 +43,35 @@ export const FileUpload = ({
       </div>
     );
   }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => {
+            onChange("");
+            {
+              edit !== true && deleteValue(imageId);
+            }
+          }}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
